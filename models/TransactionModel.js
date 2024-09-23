94d0e4b4bcb8342a571527474902bd60acc1af81
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-09-23 14:15:16 +0300

    transaction model is added

---
+++ models/TransactionModel.js
@@ -0,0 +1,44 @@
+const mongoose= require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId, // Change the type to ObjectId
+        ref: 'User', // Assuming there is a User model
+        required: true
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId, // Change the type to ObjectId
+        ref: 'User', // Assuming there is a User model
+        required: true
+    },
+    references: {
+        type: String
+    },
+    type: {
+        type: String,
+        enum: ['local Transfer',"local Transfer from person who book the room to the landlord","book commision from person who book the room","local Transfer from person who add the room"],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed'],
+        default: 'pending',
+        required: true
+    },
+    // Add more fields based on your requirements
+
+    // Timestamps for created and updated dates
+   
+},
+{
+    timestamps: true
+}
+);
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+export default Transaction;
