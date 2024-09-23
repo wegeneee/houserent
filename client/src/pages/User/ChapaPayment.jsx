61d8db72d24eedda0512e4ceb60db82f71537599
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-09-23 20:50:09 +0300

    Refactor Wallet component to add ChapaPayment feature

---
+++ client/src/pages/User/ChapaPayment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChapaPayment() {
+  return (
+    <div>
+        <button>deposit</button>
+    </div>
+  )
+}
+
+export default ChapaPayment
--- client/src/pages/Wallet.jsx
+++ client/src/pages/Wallet.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Balance from '../pages/User/Balance';
 import TransactionHistory from '../pages/User/TransactionHistory';
+import ChapaPayment from './User/ChapaPayment';
 
 function Wallet() {
   return (
     <div>
       <p>This is display my balance and add a balance to my account from chapa payments</p>
       <Balance />
+      <ChapaPayment/>
       <TransactionHistory />
     </div>
   );
